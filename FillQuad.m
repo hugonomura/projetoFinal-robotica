eff3f7694e63ac663c36b2dd696f4d7a96d84716
Rodrigo Mello <mello.rodrigo91@gmail.com>
2013-01-21 22:49:44 -0200

    Pintando mais ou menos o quadrante

--- FillQuad.m
+++ FillQuad.m
@@ -3,8 +3,19 @@ function i = FillQuad(xmin, ymin, img )
 %   Detailed explanation goes here
     i=0;
     while(img(xmin+i,ymin+i)==0)
-        img(xmin+i,ymin+i)=1;
-        i= i-1;
+        img(xmin+i,ymin+i)=0;
+        i = i-1;
+    end
+    
+    j = i + 1;
+    k = 0;
+    while(img(xmin+j, ymin+k)==0)
+        while(img(xmin+j,ymin+k)==0)
+            img(xmin+j, ymin+k) = 1;
+            j = j + 1;
+        end
+        k = k + 1;
+        j = i + 1;
     end
     i=img;
 end
